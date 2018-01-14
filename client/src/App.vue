02cdc6f914534d3ea86637a865a67ba28e06e6f8
kalibani <kalibani.ka@gmail.com>
2018-01-14 23:09:53 +0700

    configuration

---
+++ client/src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <header>
+      <appNavbar></appNavbar>
+    </header>
+    <main>
+      <router-view></router-view>
+    </main>
+  </div>
+</template>
+
+<script>
+import navbar from './components/navbar.vue'
+export default {
+  name: 'app',
+  components:{
+    appNavbar : navbar
+  }
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+
+main {
+  text-align: center;
+  margin-top: 40px;
+}
+
+header {
+  margin: 0;
+  height: 56px;
+  padding: 0 16px 0 24px;
+  color: #ffffff;
+}
+
+header span {
+  display: block;
+  position: relative;
+  font-size: 20px;
+  line-height: 1;
+  letter-spacing: .02em;
+  font-weight: 400;
+  box-sizing: border-box;
+  padding-top: 16px;
+}
+</style>
